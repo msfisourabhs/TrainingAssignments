e95495eb478d5376b94aa036c80900ee6608a20e
msfisourabhs <sourabhs@mindfiresolutions.com>
2017-05-30 19:14:48 +0530

    Improved the Regform.html by adding more validations

--- js/RegFormJS.js
+++ js/RegFormJS.js
@@ -24,14 +24,33 @@ var city = document.getElementsByName("city");
 city[0].addEventListener("blur",checkWord);
 city[0].addEventListener("focus",clearErrors);
 
+var email = document.getElementById("mail");
+email.addEventListener("blur",checkRest)
+email.addEventListener("focus",clearErrors);
+
+//var geneder = document.getElementsByName('gender');
+var caddr = document.getElementsByName("caddress");
+caddr[0].addEventListener("blur",checkRest);
+caddr[0].addEventListener("focus",clearErrors);
+
+var paddr = document.getElementsByName("paddress");
+paddr[0].addEventListener("blur",checkRest);
+paddr[0].addEventListener("focus",clearErrors);
+
+var country = document.getElementsByName("country");
+country[0].addEventListener("blur",checkRest);
+country[0].addEventListener("focus",clearErrors);
+
+var state = document.getElementsByName("state");
+state[0].addEventListener("blur",checkRest);
+state[0].addEventListener("focus",clearErrors);
+
 
 function checkWord(){
 		var c;
 		
 		for(var i=0 ; i<this.value.length; i++)
 		{
-			
-			
 			var unicode = this.value.charCodeAt(i);
 			if((unicode >= 65 && unicode <=90) || (unicode >= 97 && unicode <= 122))
 					c = 1;
@@ -40,6 +59,7 @@ function checkWord(){
 		}
 		if(c === 1)
 			console.log('OK');
+
 		else
 		{
 			console.log('Dosen\'t contains letters');
@@ -49,50 +69,73 @@ function checkWord(){
 }
 function checkPass()
 {
-			if(pass[0].value === cpass[0].value)
-				{
+	if(pass[0].value.length === 0 || cpass[0].value.length === 0)
+	{
+		generateErrors("",pass[0]);
+		generateErrors("",cpass[0]);
+	}
+	else
+	{
+		if(pass[0].value === cpass[0].value)
+		{
 					console.log("Passwords Ok");
-				}
-			else
-				{
-					console.log("Passwords don't match");
-					generateErrors("Passwords don't match",pass[0]);
+		}
+		else
+		{
+			console.log("Passwords don't match");
+			generateErrors("Passwords don't match",pass[0]);
 				
-				}
-				return;
+		}
+		return;
+	}
 }
 function checkNumber()
 {
 	var c = 1; 
-	for(var i = 0; i<this.value.length;i++)
+	if(this.value.length === 0)
+		generateErrors("",this);
+	else if(this.value.length < 10)
+		generateErrors("Field cannot be less than 10 digits" , this);
+	else if(pno[0].value === pno[1].value)
+		generateErrors("Number should be different form primary number",pno[1]);
+	else
 	{
+		for(var i = 0; i<this.value.length;i++)
+		{
 			
 			var unicode = this.value.charCodeAt(i);
 			if(unicode >=48 && unicode <= 57)
 				c=1;
 			else 
 				c=-1;
-	}
-	if(c === 1)
-					console.log("Phone Number OK");
-	else
-	{
-						console.log("Phone Number dosen't contains number");
-						generateErrors("Phone Number dosen't contains number",this);
+		}
+		if(c === 1)
+			console.log("Phone Number OK");
+		else
+		{
+			console.log("Phone Number dosen't contains number");
+			generateErrors("Phone Number dosen't contains number",this);
+		}
 	}
 	return;
 }
 function generateErrors(errormssg , name)
 {
-			
-					var em = document.createElement("p");
-					em.appendChild(document.createTextNode(errormssg));
-			
-					name.insertAdjacentElement('beforebegin',em);
-					em.setAttribute("class" , "errors");
+	var em = document.createElement("p");
+	if(name.value === "")
+	{
+		em.appendChild(document.createTextNode("This field cannot be empty"));
+		console.log(name.value,'1');
+	}
+	else	
+	{
+		em.appendChild(document.createTextNode(errormssg));
+		console.log(name.value,'2');
+	}
+	name.insertAdjacentElement('beforebegin',em);
+	em.setAttribute("class" , "errors");
 		
-			
-			return;
+	return;
 }
 function clearErrors()
 {
@@ -114,7 +157,8 @@ function clearErrors()
 			}
 			return;
 }
-function createCaptcha(){
+function createCaptcha()
+{
 	var cap = document.getElementById("captcha");
 	var opvalue = function(){
 		var op = parseInt(Math.random() * (5 - 1) + 1);
@@ -127,19 +171,56 @@ function createCaptcha(){
 		else
 			return "/";
 	};
-	cap.innerHTML = parseInt(Math.random()*10) + opvalue() + parseInt(Math.random()*10);
-	
+	cap.innerHTML = parseInt(Math.random()*10 + 1) + opvalue() + parseInt(Math.random()*10 + 1);
+	document.getElementsByName('capval')[0].value = "";
 	
 }
 function checkCaptcha()
 {
-	if(parseInt(document.getElementsByName("capval")[0].value) === eval(document.getElementById("captcha").innerHTML))
+	if(parseInt(parseInt(document.getElementsByName("capval")[0].value)) === parseInt(eval(document.getElementById("captcha").innerHTML)))
 	{
 		document.getElementsByTagName("button")[2].removeAttribute('disabled');
+		document.getElementById("capmssg").innerHTML = " &#10004 Captcha Input was Ok.You can Sign Up Now";
+		document.getElementById("capmssg").style.color = "Green";
+		//document.getElementById("capmssg").style.animationName = "example";
 		
 	}
 	else
-		{}
+	{
+		document.getElementById("capmssg").innerHTML = " &#10006 Captcha input was incorrect.Please try again.";
+		document.getElementsByTagName("button")[2].setAttribute("disabled","true");
+		document.getElementById("capmssg").style.color = "Red";
+		createCaptcha();
+		
+	}
 
 }
-        
+function validate()
+{
+	var c = 0;
+	var lbl = document.getElementsByTagName('label');
+	for(var i = 0;i<lbl.length;i++)
+	{
+		if(lbl[i].className === "required")
+		{
+			if(lbl[i].nextElementSibling.className === "errors")
+			{
+				document.getElementById("val").innerHTML = "There were errors in your submission";
+				document.getElementById("val").setAttribute("class","valmssg");
+				document.body.scrollTop = "0";
+				c++;
+			}
+			
+		}
+	}
+	if(c === 0)
+		return true;
+	else
+		return false;
+}
+function checkRest()
+{
+	if(this.value.length === 0)
+		generateErrors("",this);
+	
+}    
